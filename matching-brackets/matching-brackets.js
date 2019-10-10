29030ddbff70ea729bb171efa97da11c848b2b6d
brtrduon <brianduongt@gmail.com>
2019-10-10 10:13:48 -0700

    matching-brackets

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -3,6 +3,61 @@
 // convenience to get you started writing code faster.
 //
 
-export const matchingBrackets = () => {
-  throw new Error("Remove this statement and implement this function");
+export const matchingBrackets = (bStr) => {
+  if (bStr.length % 2 !== 0) {
+    return false
+  }
+
+  else {
+    let cBrack = 0
+    let sBrack = 0
+    let parens = 0
+
+    for (let i of bStr) {
+      if (i === '{') {
+        cBrack++
+      }
+
+      else if (i === '}') {
+        cBrack--
+
+        if (cBrack === 0 && sBrack !== 0) {
+          return false
+        }
+
+        else if (cBrack === 0 && parens !== 0) {
+          return false
+        }
+      }
+
+      else if (i === '[') {
+        sBrack++
+      }
+
+      else if (i === ']') {
+        sBrack--
+      }
+
+      else if (i === '(') {
+        parens++
+      }
+      
+      else if (i === ')') {
+        parens--
+      }
+
+
+      if (cBrack < 0 || sBrack < 0 || parens < 0) {
+        return false
+      }
+    }
+
+    if (cBrack === 0 && sBrack === 0 && parens === 0) {
+      return true
+    }
+
+    else {
+      return false
+    }
+  }
 };
